1559c1f750d306dc80607bd62a89c6eb2a79d6b5
Tranthanhhai281 <53221090+Tranthanhhai281@users.noreply.github.com>
2021-03-22 10:25:51 +0700

    update

---
+++ HRManagers/src/main/java/com/example/HRManagers/bol/UserBO.java
@@ -0,0 +1,24 @@
+package com.example.HRManagers.bol;
+
+import com.example.HRManagers.dal.DAO;
+import com.example.HRManagers.entities.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserBO {
+    public boolean authorization(User u) {
+        String sql = "select * from users where username='" + u.getUserName() + "' and userpassword='" + u.getUserPassword() + "'";
+        DAO dao = new DAO();
+        try {
+            ResultSet rs = dao.executeQuery(sql);
+            if (!rs.next()) {
+                return false;
+            }
+        } catch (SQLException ex) {
+            //SinhNX_Lib.getLogger(this.getClass().getName()).error("Can't get data from database. " + ex.toString());
+
+            return false;
+        }
+        return true;
+    }
+}

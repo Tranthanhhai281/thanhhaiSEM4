cfcdf12e3a0d4043c406aab9562e6bd000b8cdc5
Tranthanhhai281 <53221090+Tranthanhhai281@users.noreply.github.com>
2021-04-02 09:58:09 +0700

    update

---
+++ IASF/Pratical-Spring-master/src/main/java/aptech/fpt/spring/SpringDemoApplication.java
@@ -0,0 +1,49 @@
+package aptech.fpt.spring;
+
+import aptech.fpt.spring.filehandle.UploadController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+
+
+import java.io.File;
+import java.util.Locale;
+
+@SpringBootApplication
+public class SpringDemoApplication implements WebMvcConfigurer {
+
+    private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
+
+    public static void main(String[] args) {
+        new File(UploadController.UPLOADED_FOLDER).mkdir();
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci
+                = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}

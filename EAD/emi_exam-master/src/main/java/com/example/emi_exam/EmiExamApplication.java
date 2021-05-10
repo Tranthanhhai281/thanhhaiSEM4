1b7a1a4a7a949286f826763a1f22f63823e1816a
Tranthanhhai281 <53221090+Tranthanhhai281@users.noreply.github.com>
2021-05-10 10:27:58 +0700

    update

---
+++ EAD/emi_exam-master/src/main/java/com/example/emi_exam/EmiExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.emi_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmiExamApplication.class, args);
+    }
+
+}
---
+++ EAD/emi_exam-master/src/test/java/com/example/emi_exam/EmiExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emi_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmiExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
